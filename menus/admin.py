f6f836a4e8aceb3d4e526ab119a4f5a18f600476
eehmull <eehmull@gmail.com>
2018-11-22 01:53:53 +0100

    Inserción de platos en menus

--- menus/admin.py
+++ menus/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib import admin
-from .models import Plato, PlatoAdmin, Menu, MenuAdmin
+from menus.models import Plato, PlatoAdmin, Menu, MenuAdmin
 # Register your models here.
 admin.site.register(Plato, PlatoAdmin)
 admin.site.register(Menu, MenuAdmin)
--- restaurante/settings.py
+++ restaurante/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurante'
+    'menus',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
